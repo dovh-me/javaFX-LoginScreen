6ef713ad88862ffcb8be68b27fb6fd6f7691dbc8
dovh-me <dovh.me@gmail.com>
2021-02-15 21:02:35 +0530

    [MODIFY]removed the date picker from the signup page since it doesn't allow background color customizations. [ADD]Added close button with the general styling from the styles.css

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -1,8 +1,14 @@
 package sample;
 
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.stage.Stage;
+import javafx.stage.StageStyle;
+
+import java.io.IOException;
 
 public class Controller {
     @FXML
@@ -17,5 +23,18 @@ public class Controller {
     public void closeBtn(){
         Stage thisStage = ((Stage) loginBtn.getScene().getWindow());
         thisStage.close();
+    }
+
+    public void signUpBtn(){
+        try {
+            Parent root = FXMLLoader.load(getClass().getResource("signup.fxml"));
+            Stage newStage = new Stage();
+            newStage.initStyle(StageStyle.UNDECORATED);
+            newStage.setScene(new Scene(root, 500,600));
+            newStage.show();
+            closeBtn();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
