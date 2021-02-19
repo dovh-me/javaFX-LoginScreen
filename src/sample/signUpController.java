b31bd28ef262d402edea5cde34f1d5748e726469
dovh-me <dovh.me@gmail.com>
2021-02-19 17:25:32 +0530

    [MODIFY]Added the second page of the signup form in the same FXML file and the naviagtion buttons for the pages.

--- src/sample/signUpController.java
+++ src/sample/signUpController.java
@@ -2,14 +2,25 @@ package sample;
 
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
+import javafx.scene.layout.AnchorPane;
 import javafx.stage.Stage;
 
 public class signUpController {
     @FXML
     Button closeBtn;
 
+    @FXML
+    AnchorPane page2;
+
     public void closeWindow(){
         Stage thisStage = (Stage) closeBtn.getScene().getWindow();
         thisStage.close();
+    }
+
+    public void nextPage(){
+        page2.setVisible(true);
+    }
+    public void prevPage(){
+        page2.setVisible(false);
     }
 }
